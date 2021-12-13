724c0a40cf3a436f5a2b2ebb7976866a4e82624d
Kristýna Švarcová <redyblackie@gmail.com>
2021-12-13 20:43:28 +0100

    Add an offline cache

--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.squareup.moshi.JsonClass
 
@@ -50,6 +51,20 @@ data class NetworkVideo(
         val updated: String,
         val thumbnail: String,
         val closedCaptions: String?)
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo(
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }
+}
 
 /**
  * Convert Network results to database objects
